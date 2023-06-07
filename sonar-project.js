ec9fb7be96c7b48248bed81389d3563fe2ef46b8
Raybest93 <122949581+Raybest93@users.noreply.github.com>
2023-06-07 16:00:03 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://http://3.93.199.192:9000/',
+  serverUrl: 'http://http://172.31.24.185:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
